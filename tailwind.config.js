01b7919a8aa876375e5b19ea9ca2b900111f2daa
WavyWare <jflortech@gmail.com>
2024-06-16 15:56:48 +0200

    Added TailwindCSS config files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './src/**/*.{html,php}',
+      './**/*.{html,php}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
